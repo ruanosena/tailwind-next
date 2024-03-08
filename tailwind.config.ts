cf04884d948c79e26f290b54e693664bec5bce5d
ruanosena <ruosena@hotmail.com>
2024-03-08 10:36:55 -0300

    animações e acessibilidade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
         profile: 'max-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
       borderWidth: {
         6: '6px',
       },
@@ -18,8 +19,26 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+
       maxWidth: {
         app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
       },
     },
   },
