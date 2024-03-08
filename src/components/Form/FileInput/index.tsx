7d79cd4eea39fa44cd5cbb728d793048b8a3fbc3
ruanosena <ruosena@hotmail.com>
2024-03-08 09:43:43 -0300

    formulário

---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,7 @@
+import { Root } from './Root'
+import { Control } from './Control'
+import { Trigger } from './Trigger'
+import { ImagePreview } from './ImagePreview'
+import { FileList } from './FileList'
+
+export { Root, Control, ImagePreview, Trigger, FileList }
